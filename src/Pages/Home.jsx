68109ce1efaedb8025100ce5bb0e5d10e1060817
krishna99048 <sheladiyakrishna17@gmail.com>
2026-04-01 17:47:11 +0530

    Deploy website

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,7 +7,7 @@ import CategoryBar from '../Components/Home/CategoryBar'
 import Offer from '../Components/Home/Offer'
 import DealBar from '../Components/Home/DealBar'
 import DiscountBar from '../Components/Home/DiscountBar'
-import Newsletter from '../Components/Home/NewsLatter'
+import Newsletter from '../Components/Home/Newslatter'
 
 const Home = () => {
   return (
